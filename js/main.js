1365aa40d30f7cc95b6605e1a660ec6313ff87e9
XolaniMgube <xnmgube@gmail.com>
2020-09-28 17:06:43 +0200

    created the addClass function

--- js/main.js
+++ js/main.js
@@ -14,6 +14,20 @@ let toggleNav = function(){
 
 		toggleNavStatus = false;
 	}
-
-
 }
+
+function  addClass() {
+	let element = document.getElementsByClassName("navigation")
+	for (let i = 0; i < element.length; i++) {
+		element[i].addEventListener("click", function() {
+			let current = document.getElementsByClassName("active")
+			if (current.length == 1) {
+				current[0].className = current[0].className.replace(" active", "")
+			}
+			this.className += " active"
+		})
+	}
+}
+
+addClass()
+
