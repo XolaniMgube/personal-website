374723cdf620b543b294cc37eb336029821ccbd3
XolaniMgube <xnmgube@gmail.com>
2020-12-13 20:21:05 +0200

    need to works on the skills

--- js/main.js
+++ js/main.js
@@ -1,33 +1,117 @@
-let toggleNavStatus = false;
+/** @format */
+// let skillsDiv = document.querySelector(".skills").innerHTML;
 
-let toggleNav = function(){
-	let getNav = document.querySelector(".nav");
 
-	if (toggleNavStatus === false){
-		getNav.style.overflow = "visible";
+// let hoverEffect = () => {
+//   let skill = document.querySelectorAll(".skill");
+//   let skillsDiv = document.querySelector(".skills").innerHTML
 
-		toggleNavStatus = true; 
-		} 
+//   for (let i = 0; i < skill.length; i++) {
 
-	else if (toggleNavStatus === true){
-		getNav.style.overflow = "hidden";
+//     skill[i] = '<img src="images/html.png">'; 
 
-		toggleNavStatus = false;
-	}
+//     skill[i].addEventListener("mouseover", () => {
+//       skill[i].style.backgroundColor = "red";
+//       skillsDiv = "<div>xolani</div>";
+//     });
+//   }
+
+//   document.querySelector(".skills").innerHTML = skillsDiv;
+
+// };
+
+
+
+hoverEffect();
+
+const navFunctionality = () => {
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
+  const burger = document.querySelector(".burger");
+
+  let animateLinks = () => {
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.2
+        }s`;
+      }
+    });
+  };
+
+  let burgerToggle = () => {
+    burger.classList.toggle("toggle");
+  };
+
+  let navToggle = () => {
+    nav.classList.toggle("nav-active");
+  };
+
+  // Making navLinks go away after clicking on one link && burger menu transformation
+  if (window.innerWidth <= 900) {
+    nav.addEventListener("click", () => {
+      navToggle();
+      animateLinks();
+      burgerToggle();
+    });
+  }
+
+  // Making navLinks appear and disappear when clicking the burger menu && burger menu transformation
+  burger.addEventListener("click", () => {
+    navToggle();
+    animateLinks();
+    burgerToggle();
+  });
+};
+
+// Styling the nav links when they are active
+function activeNavLinks() {
+  let element = document.getElementsByClassName("navigation");
+  for (let i = 0; i < element.length; i++) {
+    element[i].addEventListener("click", function () {
+      let current = document.getElementsByClassName("active");
+      if (current.length == 1) {
+        current[0].className = current[0].className.replace(" active", "");
+      }
+      this.className += " active";
+    });
+  }
 }
 
-function  addClass() {
-	let element = document.getElementsByClassName("navigation")
-	for (let i = 0; i < element.length; i++) {
-		element[i].addEventListener("click", function() {
-			let current = document.getElementsByClassName("active")
-			if (current.length == 1) {
-				current[0].className = current[0].className.replace(" active", "")
-			}
-			this.className += " active"
-		})
-	}
-}
+let invockingFunctions = () => {
+  //   window.onscroll = () => scroll();
+  navFunctionality();
+  activeNavLinks();
+};
 
-addClass()
+invockingFunctions();
 
+// Smooth scrooll effect on cross browser in JQuery
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+});
